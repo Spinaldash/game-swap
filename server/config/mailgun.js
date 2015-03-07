e4f3bea475203b6bb6c59304f2aff3a8a43c5318
cnichols <mellowdious@mail.com>
2015-03-07 13:46:36 -0800

    refactored mailgun

--- server/config/mailgun.js
+++ server/config/mailgun.js
@@ -7,8 +7,7 @@ var mg = new Mailgun(process.env.MG_KEY);
 var sender = 'noreply@game-swap.com';
 
 var signupSubject = 'Welcome to Game-Swap!';
-var signupText = "Hi " + user.userName +
-      "!\n\nYou've signed up to Game-Swap!  Game-Swap is an easy way to take games you own, and trade them for games you want.\n\n" + 
+var signupText = "!\n\nYou've signed up to Game-Swap!  Game-Swap is an easy way to take games you own, and trade them for games you want.\n\n" + 
       "Log in and start trading now!\n\n--The Game-Swap Team";
 
 var emailCB = function(err) {
@@ -24,6 +23,6 @@ module.exports = {
   signup: function(user) {
     console.log('*****USER signing up:', user.email);
     console.log('signupEmail called');
-    mg.sendText(sender, user.email, signupSubject, signupText, emailCB);
+    mg.sendText(sender, user.email, signupSubject, "Hi " + user.userName + signupText, emailCB);
   }
 };
