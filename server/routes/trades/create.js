471acdfd64055d358e170e309fac185d0fe4ffa7
cnichols <mellowdious@mail.com>
2015-03-09 11:35:03 -0700

    wip pending states

--- server/routes/trades/create.js
+++ server/routes/trades/create.js
@@ -7,6 +7,7 @@ module.exports = {
   handler: function(request, reply) {
     var newTrade = new Trade(request.payload);
     Trade.create(newTrade, function(trade) {
+      console.log('ROUTESTRADECREATE trade:', trade);
       Trade.findById(trade._id).populate('item1 item2 user1 user2').exec(function(err, populatedTrade) {
         if(!err) {
           mailgun.tradeOffered(populatedTrade);
